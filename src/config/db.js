69e66622fe608a8f1b1e89d5cda593cde8a31861
Vinoth Kumar S <107671734+VinothKumar2911@users.noreply.github.com>
2025-12-23 13:58:59 +0530

    Initial backend commit

--- src/config/db.js
+++ src/config/db.js
@@ -1,10 +1,32 @@
+// const mysql = require('mysql2/promise');
+
+// const pool = mysql.createPool({
+//   host: 'localhost',
+//   user: 'root',
+//   password: 'root',
+//   database: 'hospital_app'
+// });
+
+// module.exports = pool;
+// const mysql = require('mysql2/promise');
+
+// const pool = mysql.createPool({
+//   host: 'localhost',
+//   user: 'root',
+//   password: 'root',
+//   database: 'hospital_app'
+// });
+
+// module.exports = pool;
+
+
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'root',
-  database: 'hospital_app'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 module.exports = pool;
