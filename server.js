d60d92b28584d6861c8b7ed97096247aa48bd211
Vinoth Kumar S <107671734+VinothKumar2911@users.noreply.github.com>
2025-12-29 21:57:11 +0530

    fix: correct DB_PORT and render env usage

--- server.js
+++ server.js
@@ -1,13 +1,3 @@
-// // server.js
-// require('dotenv').config();
-
-// const app = require('./src/app');
-
-// const PORT = process.env.PORT || 5000;
-
-// app.listen(PORT, () => {
-//   console.log(`🚀 Server running on port ${PORT}`);
-// });
 
 
 
